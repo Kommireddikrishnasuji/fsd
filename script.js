c0cc94876326230122742f54fd772a3416961a7c
Kommireddikrishnasuji <158247592+Kommireddikrishnasuji@users.noreply.github.com>
2025-04-07 22:13:23 +0530

    cat-bulb project

--- script.js
+++ script.js
@@ -1,45 +1,22 @@
+document.getElementById("offSwitch").addEventListener("click", switchOff);
+document.getElementById("onSwitch").addEventListener("click", switchOn);
 
-let nextDom = document.getElementById('next');
-let prevDom = document.getElementById('prev');
-
-let carouselDom = document.querySelector('.carousel');
-let SliderDom = carouselDom.querySelector('.carousel .list');
-let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
-let timeDom = document.querySelector('.carousel .time');
-
-
-thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 2000;
-
-nextDom.onclick = function(){
-    showSlider('next');    
+function switchOff() {
+    document.getElementById("bulbImage").src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent = "Switched Off";
+    document.getElementById("onSwitch").style.backgroundColor = "#22c55e";
+    document.getElementById("offSwitch").style.backgroundColor = "#cbd2d9";
 }
 
-prevDom.onclick = function(){
-    showSlider('prev');    
-}
-let runTimeOut;
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
-    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if(type === 'next'){
-        SliderDom.appendChild(SliderItemsDom[0]);
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        carouselDom.classList.add('next');
-
-        
-    }else{
-        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-        carouselDom.classList.add('prev');
-
-    }
-    clearTimeout(runTimeOut);
-    runTimeOut = setTimeout(() => {
-        carouselDom.classList.remove('next');
-        carouselDom.classList.remove('prev');
-    }, timeRunning);
-
+function switchOn() {
+    document.getElementById("bulbImage").src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src =
+        "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("switchStatus").textContent = "Switched On";
+    document.getElementById("offSwitch").style.backgroundColor = "#e12d39";
+    document.getElementById("onSwitch").style.backgroundColor = "#cbd2d9";
 }
